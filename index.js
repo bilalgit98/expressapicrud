7b28c76df6938a86327f50e109dd5c1c13bbd438
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-10-17 15:11:20 +0100

    Body Parser

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+//init express application
+const app = express();
+
+//specify port
+const PORT = 5000;
+
+//init bodyparser middleware
