3bc4fc17ca6389ea44b5a6ab4085e20246a11320
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-10-17 17:39:08 +0100

    JSON Formatter

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,25 @@ import express from "express";
 
 //import router
 const router = express.Router();
+const users = [
+  {
+    firstName: "john",
+    lastName: "doe",
+    age: 25,
+  },
+  {
+    firstName: "jane",
+    lastName: "doe",
+    age: 23,
+  },
+];
 
 //init router ( all routes here are starting with "/users" ("/"))
 router.get("/", (req, res) => {
+  console.log(users);
   res.send("Hello");
 });
+
+router.post("/", (req, res) => {});
 
 export default router;
