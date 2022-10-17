0e15d1e21e05126c2e653f650fc4d8acfaa1468b
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-10-17 15:22:23 +0100

    Callback

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ const PORT = 5000;
 //init bodyparser middleware
 app.use(bodyParser.json());
 
+//creating different routes
+app.get("/", (req, res) => {
+  res.send("hello from homepage");
+});
+
 //listen for incoming request
 app.listen(PORT, () =>
   console.log(`Server is running on port: http://localhost:${PORT}`)
