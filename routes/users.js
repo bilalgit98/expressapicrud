400be46082fe1d420a180f72b7b9d88df6bcdef3
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-10-18 19:16:41 +0100

    Postman

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,42 @@
+import e from "express";
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
 
 //import router
 const router = express.Router();
-const users = [
-  {
-    firstName: "john",
-    lastName: "doe",
-    age: 25,
-  },
-  {
-    firstName: "jane",
-    lastName: "doe",
-    age: 23,
-  },
-];
+
+//mock database
+const users = [];
 
 //init router ( all routes here are starting with "/users" ("/"))
 router.get("/", (req, res) => {
-  console.log(users);
-  res.send("Hello");
+  res.send(users);
 });
 
-router.post("/", (req, res) => {});
+//post
+router.post("/", (req, res) => {
+  //adding the request to the mock database
+  const user = req.body;
+  //using uuid
 
+  console.log({ ...user, id: uuidv4() });
+
+  users.push({ ...user, id: uuidv4() });
+
+  res.send(`user with the name "${user.firstName}" has been added!`);
+});
+
+//.get
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  //finding the user id
+  const foundUser = users.find((user) => user.id === id);
+  res.send(foundUser);
+});
+
+//deleting users
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+});
 export default router;
